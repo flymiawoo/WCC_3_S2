7542e14bd8a3953342ef982396b4314e53812e10
flymiawoo <azertyqwerton@gmail.com>
2023-07-08 16:10:11 +0300

    bouton menu ok pour desktop

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,17 @@
 const menu = document.getElementById('menu-icon')
 const menuItem = document.getElementById('menu-container');
 
+let indicateur=false;
+
 menu.addEventListener('click', ()=>{
-    console.log('yesss')
-    menuItem.classList.add('show')
+    if (!indicateur){
+        console.log('yesss?..., fa ina ko ty zavatra ty ohh!!?')
+        menuItem.classList.add('show')
+        indicateur=true;
+    }else{
+        menuItem.classList.remove('show');
+        indicateur=false;
+    }
+    // zay fa mipotra sy miafina le menu
+    // fa mbola tokony amboarina :)  ♥ ☻
 })
