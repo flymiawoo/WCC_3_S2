6f9ca90b9993060179522a19684f78c95ca433d9
flymiawoo <azertyqwerton@gmail.com>
2023-07-08 20:56:07 +0300

    resolution de conflit

--- src/js/app.js
+++ src/js/app.js
@@ -2,35 +2,41 @@ const menu = document.getElementById('menu-icon')
 const menuItem = document.getElementById('menu-container');
 const divMenu = document.querySelector('.menu');
 
+let windowX;
+
 let indicateur=false;
 
 menu.addEventListener('click', ()=>{
     if (!indicateur){
         console.log('yesss?..., fa ina ko ty zavatra ty ohh!!?')
         menuItem.classList.add('show')
-        divMenu.style.top="100%";
+        divMenu.style.top="100px";
         indicateur=true;
-    }else{
+    }else {
+        close()
+    }
+    // zay fa mipotra sy miafina le menu
+    // fa mbola tokony amboarina :)  ♥ ☻
+})
+
+function close(){
         divMenu.style.top="-100%";
         indicateur=false;
         setTimeout(()=>{
             menuItem.classList.remove('show');
         },100)
     }
-    // zay fa mipotra sy miafina le menu
-    // fa mbola tokony amboarina :)  ♥ ☻
-})
 
+function getClass(x){
+    return document.querySelector(x);
+}
+
+function noText(it){
+    
+}
 
 // manao animation anle zaza fa tsy mety tafavoka
 let zaza=document.querySelector(".zaza");
-
-function animerZaza(){
-    let x=0
     zaza.style.display="flex";
-    zaza.style.left="50%";
-}
+    zaza.style.left="120px";
 
-setTimeout(()=>{
-    animerZaza();
-}, 1000)
